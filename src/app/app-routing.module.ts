52e9848399c00d90bc1e0161ce745d03e6457e36
jacksendall <jacksendall@gmail.com>
2020-04-26 20:12:45 +0100

    summary
    descrition

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { PricingComponent } from './pricing/pricing.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { AccountManagementComponent } from './account-management/account-management.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 const routes: Routes = [
@@ -11,6 +12,7 @@ const routes: Routes = [
   {path: 'about', component: AboutComponent},
   {path: 'pricing', component: PricingComponent, data: { animation: 'Fade' } },
   {path: 'account-management', component: AccountManagementComponent, data: { animation: 'Fade' } },
+  {path: 'contact', component: ContactComponent, data: { animation: 'Fade' } },
   //otherwise redirect to home
   { path: '**', redirectTo: '' }
 ];
